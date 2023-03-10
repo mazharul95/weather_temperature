1f5cb5f9560d8d37e2aea9f830f46d94813d7110
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-11 02:48:18 +0600

    Get API key and load temperature data by city name

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const API_KEY = `4b0473d71ee0e97f36acfb92a00c6726`;
-
 const loadTemperature = city =>{
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log())
+    .then(data => displayTemperature(data))
 }
+
+const displayTemperature =data =>{
+    const temperature =document.getElementById('temperature');
+    temperature.innerText = data.main.temp;
+    
+
+}
+
+
+loadTemperature('dhaka');
