5257935a5de3b453c01b2c2d98baecdc4f4a0b13
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-11 03:27:08 +0600

    Display temperature, city and weather condition

--- js/app.js
+++ js/app.js
@@ -6,12 +6,29 @@ const loadTemperature = city =>{
     .then(data => displayTemperature(data))
 }
 
-const displayTemperature =data =>{
-    const temperature =document.getElementById('temperature');
-    temperature.innerText = data.main.temp;
+const displayTemperature = data =>{
+    //const temperature =document.getElementById('temperature');
+    //console.log(data.main.temp);
+    //temperature.innerText = data.main.temp;
     
-
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+    console.log(data.weather[0].main);
 }
 
+const setInnerTextById = (id,text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+
+document.getElementById('btn-search').addEventListener('click',function(){
+    const searchField = document.getElementById('search-field')
+    const city = searchField.value;
+    //set city in search
+    document.getElementById('city').innerText = city;
+
+    loadTemperature(city);
+
+})
 
 loadTemperature('dhaka');
